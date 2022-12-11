54a62c6dd87ebffd24a0fb03caeb4f287df52c1c
hoaminzy <48762609+student1060632fpt@users.noreply.github.com>
2022-12-12 00:51:07 +0700

    Feat: add package near and add near wallet

---
+++ Process.md
@@ -0,0 +1,8 @@
+# Todo 12/12
+-setup vercel lỗi 
+```
+No Output Directory named "build" found after the Build completed. You can configure the Output Directory in your Project Settings.
+```
+cách giải
+https://github.com/electerious/Ackee/blob/master/docs/Get%20started.md#with-vercel
+
---
+++ src/shared/near/near-interface.js
@@ -0,0 +1,242 @@
+import { throws } from "assert";
+import {utils} from "near-api-js";
+
+export class NFTMarketplace {
+    constructor({contractId, wallet}) {
+        this.contractId = contractId;
+        this.wallet = wallet;
+    }
+
+    async mintNFT(tokenId, metadata, receiverId) {
+        return await this.wallet.callMethod({
+            contractId: this.contractId,
+            method: "nft_mint",
+            args: {
+                token_id: tokenId,
+                metadata,
+                receiver_id: receiverId
+            },
+            deposit: utils.format.parseNearAmount("0.01") // Deposit 0.01 NEAR
+        })
+    }
+    async nftTransfer(receiverId, tokenId, approvalId = 0, memo = "") {
+        return await this.wallet.callMethod({
+            contractId: this.contractId,
+            method: "nft_transfer",
+            args: {
+                receiver_id: receiverId,
+                token_id: tokenId,
+                approval_id: approvalId,
+                memo
+            },
+            deposit: 1 // Deposit 1 yoctoNEAR
+        })
+    }
+
+    async nftToken(tokenId) {
+        return await this.wallet.viewMethod({
+            contract: this.contractId,
+            method: "nft_token",
+            args: {
+                token_id: tokenId
+            }
+        })
+    }
+
+    async nftTokenForOwner(accountId, fromIndex = "0", limit = 100) {
+        console.log("Account: ", accountId);
+        return await this.wallet.viewMethod({
+            contractId: this.contractId,
+            method: "nft_tokens_for_owner",
+            args: {
+                account_id: accountId,
+                from_index: fromIndex,
+                limit
+            }
+        })
+    }
+
+    async getSales() {
+        return await this.wallet.viewMethod({
+            contractId: this.contractId,
+            method: "get_sales",
+            args: {}
+        })
+    }
+    async buy(saleId, amount) {
+        return await this.wallet.callMethod({
+            contractId: this.contractId,
+            method: "buy",
+            args: {
+                sale_id: saleId
+            },
+            deposit: amount
+        })
+    }
+    async addSale(tokenId, price) {
+        return await this.wallet.callMethod({
+            contractId: this.contractId,
+            method: "add_sale",
+            args: {
+                token_id: tokenId,
+                price: utils.format.parseNearAmount(price)
+            },
+            deposit: 1
+        })
+    }
+}
+// ABI
+
+export class FungibleToken{
+    constructor({contractId, wallet}) {
+        this.contractId = contractId;
+        this.wallet = wallet;
+    }
+    async ft_transfer_call(receiver_id, amount, msg){
+        return await this.wallet.callMethod({
+            contractId: this.contractId,
+            method: "ft_transfer_call",
+            args: {
+                receiver_id: receiver_id,
+                amount: amount,
+                msg: msg
+            },
+            gas: 200000000000000,
+            deposit: 1
+        })
+    }
+
+    async ft_balance_of(account_id){
+        return await this.wallet.viewMethod({
+            contractId: this.contractId,
+            method: "ft_balance_of",
+            args: {
+                account_id: account_id,
+            },
+        })
+    }
+
+    async ft_metadata(){
+        return await this.wallet.viewMethod({
+            contractId: this.contractId,
+            method: "ft_metadata",
+            args: {},
+        })
+    }
+}
+
+export class veFungibleToken{
+    constructor({contractId, wallet}) {
+        this.contractId = contractId;
+        this.wallet = wallet;
+    }
+    async ft_transfer_call(receiver_id, amount, msg){
+        return await this.wallet.callMethod({
+            contractId: this.contractId,
+            method: "ft_transfer_call",
+            args: {
+                receiver_id: receiver_id,
+                amount: amount,
+                msg: msg
+            },
+            gas: 200000000000000,
+            deposit: 1
+        })
+    }
+
+    async ft_balance_of(account_id){
+        return await this.wallet.viewMethod({
+            contractId: this.contractId,
+            method: "ft_balance_of",
+            args: {
+                account_id: account_id,
+            },
+        })
+    }
+
+    async ft_metadata(){
+        return await this.wallet.viewMethod({
+            contractId: this.contractId,
+            method: "ft_metadata",
+            args: {},
+        })
+    }
+
+    async ft_total_supply(){
+        return await this.wallet.viewMethod({
+            contractId: this.contractId,
+            method: "ft_total_supply",
+            args: {},
+        })
+    }
+    async get_total_staked(){
+        return await this.wallet.viewMethod({
+            contractId: this.contractId,
+            method: "get_total_staked",
+            args: {},
+        })
+    }
+    async get_list_jobs(){
+        return await this.wallet.viewMethod({
+            contractId: this.contractId,
+            method: "get_list_jobs",
+            args: {},
+        })
+    }
+
+}
+
+export class VotingContract{
+    constructor({contractId, wallet}) {
+        this.contractId = contractId;
+        this.wallet = wallet;
+    }
+    async show_results(poll_id){
+        return await this.wallet.viewMethod({
+            contractId: this.contractId,
+            method: "show_results",
+            args: {
+                poll_id: poll_id,
+            },
+        })
+    }
+    async show_list_voting(){
+        return await this.wallet.viewMethod({
+            contractId: this.contractId,
+            method: "show_list_voting",
+            args: {},
+        })
+    }
+
+    async vote(poll_id, votes){
+        return await this.wallet.callMethod({
+            contractId: this.contractId,
+            method: "vote",
+            args: {
+                poll_id: poll_id,
+                votes: votes,
+            },
+        })
+    }
+}
+
+export class FaucetContract{
+    constructor({contractId, wallet}) {
+        this.contractId = contractId;
+        this.wallet = wallet;
+    }
+    async ft_request_funds(ft_contract_id, receiver_id ,amount){
+        return await this.wallet.callMethod({
+            contractId: this.contractId,
+            method: "ft_request_funds",
+            args: {
+                ft_contract_id: ft_contract_id,
+                receiver_id: receiver_id,
+                amount: amount
+            },
+            deposit: 1,
+        })
+    }
+}
+
+
