5a2ec392db4026d39bdccea43ce1f5468a88f543
hoaminzy <48762609+student1060632fpt@users.noreply.github.com>
2022-12-15 16:32:10 +0700

    Fix: complete project

--- src/frontend/components/common/ListProjectManagement.jsx
+++ src/frontend/components/common/ListProjectManagement.jsx
@@ -195,19 +195,124 @@ const ListProjectManagement = ({ status = "" }) => {
       </div>
     );
   };
+  const renderListProjectIsComplete = (project) => {
+    const freelancerHiredStatus = project.freelancers.find((freelancer) => {
+      console.log({ freelancer }, "freelancer.creator[0]");
+      return freelancer?.creatorConfirm;
+    });
+    console.log(freelancerHiredStatus);
+    const freelancerHired = freelancerHiredStatus.name;
+    console.log({ freelancerHired });
+    return (
+      <div className="my-projects-list">
+        <div className="row">
+          <div className="col-lg-10 flex-wrap">
+            <div className="projects-card flex-fill">
+              <div className="card-body">
+                <div className="projects-details align-items-center">
+                  <div className="project-info">
+                    <span>{project.creator_id}</span>
+                    <h2>Website Designer Required For Directory Theme</h2>
+                    <div className="customer-info">
+                      <ul className="list-details">
+                        <li>
+                          <div className="slot">
+                            <p>Price type</p>
+                            <h5>Fixed</h5>
+                          </div>
+                        </li>
+                        <li>
+                          <div className="slot">
+                            <p>Location</p>
+                            <h5>
+                              <img src={Flags_en} height={13} alt="Lang" /> UK
+                            </h5>
+                          </div>
+                        </li>
+                        <li>
+                          <div className="slot">
+                            <p>Expiry</p>
+                            <h5>4 Days Left</h5>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="project-hire-info">
+                    <div className="content-divider" />
+                    <div className="projects-amount">
+                      <h3>PAT {project.budget}</h3>
+                      <h5>in 12 Days</h5>
+                    </div>
+                    <div className="content-divider" />
+                    <div className="projects-action text-center">
+                      <Link to={`/view-project-detail/${project.projectId}`} className="projects-btn">
+                        View Details{" "}
+                      </Link>
+                      <h4 className="completed-badge">
+                        <span>
+                          <i className="fas fa-medal me-2" />
+                        </span>
+                        Completed
+                      </h4>
+                      <div className="rating red mt-2">
+                        <i className="fas fa-star filled" />
+                        <i className="fas fa-star filled" />
+                        <i className="fas fa-star filled" />
+                        <i className="fas fa-star filled" />
+                        <i className="fas fa-star" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-2 d-flex flex-wrap">
+            <div className="projects-card flex-fill">
+              <div className="card-body p-2">
+                <div className="prj-proposal-count text-center hired">
+                  <h3>Hired</h3>
+                  <img src={Developer_01} alt="" className="img-fluid" />
+                  <p className="mb-0">{freelancerHired}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
   const renderListProject = () => {
     //status ongoing project only
+    if (status === statusRenderListProject.complete) {
+      return listProject.map((project) => {
+        if (!!project.is_start) {
+          if(project.is_end){
+            return renderListProjectIsComplete(project);
+          }
+          return <></>;
+        }
+        
+        return <></>;
+      });
+    }
     if (status === statusRenderListProject.onGoing) {
       return listProject.map((project) => {
-        if (!!project.is_start) {
+        if (!!project.is_start && !project.is_end) {
           return renderListProjectIsStarted(project);
         }
-        return <></>
-
+        if (!!project.is_start && !!project.is_end){
+          return <></>;
+        }
+        return <></>;
       });
-    } else if (status === statusRenderListProject.pending){
+    } else if (status === statusRenderListProject.pending) {
       return listProject.map((project) => {
-        if (!!project.is_start) {
+        if (!!project.is_start && !project.is_end) {
+          return <></>;
+        }
+        if (!!project.is_start && !!project.is_end){
           return <></>;
         }
         return renderListProjectNoStarted(project);
@@ -215,8 +320,11 @@ const ListProjectManagement = ({ status = "" }) => {
     }
     //all project
     return listProject.map((project) => {
-      if (!!project.is_start) {
+      if (!!project.is_start && !project.is_end) {
         return renderListProjectIsStarted(project);
+      }
+      if (!!project.is_start && !!project.is_end){
+        return renderListProjectIsComplete(project);
       }
       //if project no start yet
       return renderListProjectNoStarted(project);
